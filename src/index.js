bb5952ba34adefec7b5672ca36e43e24906368ec
John Bartos <jbartos@jwplayer.com>
2016-11-18 10:26:15 -0500

    Using mediarecorder & converting arraybuffer to float32array

--- src/index.js
+++ src/index.js
@@ -5,22 +5,43 @@ osc.frequency = 440;
 
 const envelope = ctx.createGain();
 osc.connect(envelope);
-// envelope.connect(ctx.destination);
+envelope.connect(ctx.destination);
 
-const recorder = ctx.createMediaStreamDestination();
-envelope.connect(recorder);
+const recorderDest = ctx.createMediaStreamDestination();
+const recorder = new MediaRecorder(recorderDest.stream);
+const chunks = [];
+recorder.ondataavailable = (e) => {
+  chunks.push(e.data);
+};
+recorder.onstop = (e) => {
+  const blob = new Blob(chunks, { 'type' : 'audio/ogg; codecs=opus' });
+  const reader = new FileReader();
+  reader.addEventListener('loadend', () => {
+    const buffer = new Uint8Array(reader.result);
+    const floatArray = new Float32Array(buffer);
+    console.log(floatArray);
+  });
+  reader.readAsArrayBuffer(blob);
+  // const href = URL.createObjectURL(blob);
+  // const a = document.createElement('a');
+  // a.href = href;
+  // a.download = 'foo.ogg';
+  // a.click();
+
+};
+
+
+osc.connect(recorderDest);
 // recorder.connect(ctx.destination);
 
 // const buffer = createBuffer(2, frameCount, audioCtx.sampleRate);
 const buffer = ctx.createBuffer(2, 22050, 44100);
 buffer.buffer = recorder.stream;
 
+recorder.start();
 osc.start(0);
 
-
 setTimeout(() => {
-  console.log('stob')
   osc.stop();
-  // console.log(recorder.stream);
-  recorder.connect(ctx.destination);
-}, 2000);
+  recorder.stop();
+}, 100);
